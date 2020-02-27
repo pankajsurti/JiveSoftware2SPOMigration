b76d08195f14d74c581f327fe7dc4743ee445f9a
Pankaj <pankajsurti@users.noreply.github.com>
2020-02-27 12:00:28 -0600

    Added more scripts

---
+++ Delete_SiteCollection_recyclebin.ps1
@@ -0,0 +1,9 @@
+﻿cls 
+$sitecollectionurl = "https://{yourtenantname}.sharepoint.com/sites/commsite"
+
+$connAdmin = Connect-PnPOnline -Url https://{yourtenantname}-admin.sharepoint.com -ReturnConnection -UseWebLogin
+
+$DeletedSite = Get-PnPTenantRecycleBinItem | Where {$_.URL -eq $sitecollectionurl}
+$DeletedSite
+#Read more: https://www.sharepointdiary.com/2017/12/sharepoint-online-delete-site-collection-from-recycle-bin.html#ixzz65nBldaSz
+#Clear-PnPTenantRecycleBinItem -Url $sitecollectionurl -Force
